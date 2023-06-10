d476477fc945956d7b4fb0261c360479a6a9e9c0
Palash soni <44397045+Palash9088@users.noreply.github.com>
2023-06-10 14:32:10 +0530

    Update ValidateLoginTest.java

--- src/test/java/testscripts/ValidateLoginTest.java
+++ src/test/java/testscripts/ValidateLoginTest.java
@@ -1,3 +1,9 @@
+/**  
+* @author Palash Soni
+* https://www.linkedin.com/in/Palash9088
+* https://github.com/Palash9088
+* 
+*/
 package testscripts;//import java.util.*;
 
 import org.apache.log4j.Logger;
